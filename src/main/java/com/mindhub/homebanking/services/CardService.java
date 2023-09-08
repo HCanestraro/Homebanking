3b21e09b4b51293735ec76b8d12171b02037d3a7
HCanestraro <123335461+HCanestraro@users.noreply.github.com>
2023-09-08 04:08:42 -0300

    Delete src/main/java/com/mindhub/homebanking/services directory

--- src/main/java/com/mindhub/homebanking/services/CardService.java
+++
@@ -1,8 +0,0 @@
-package com.mindhub.homebanking.services;
-import com.mindhub.homebanking.models.CardColor;
-import com.mindhub.homebanking.models.CardType;
-import org.springframework.security.core.Authentication;
-
-public interface CardService {
-	void createCard(Authentication authentication, CardType cardType, CardColor cardColor) throws Exception;
-}
--- src/main/java/com/mindhub/homebanking/services/TransactionService.java
+++
@@ -1,7 +0,0 @@
-package com.mindhub.homebanking.services;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestParam;
-
-public interface TransactionService {
-	void create(Authentication authentication, double amount, String description, String fromAccountNumber, String toAccountNumber) throws Exception;
-}
