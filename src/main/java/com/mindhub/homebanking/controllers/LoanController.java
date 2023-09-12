7357c63eda7ca43461adfde5ef828559fe20930d
HCanestraro <HCanestraro@users.noreply.github.com>
2023-09-12 18:55:00 -0300

    Testing spring9 v2.04 PostgreSQL & Dockerfile

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -10,11 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.transaction.Transactional;
-import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api")
@@ -23,17 +22,68 @@ public class LoanController {
 	private ClientLoanService clientLoanService;
 	@Autowired
 	private ClientService clientService;
+	
 	@Autowired
 	private LoanService loanService;
+	
 	@Autowired
 	private AccountRepository accountService;
 	@Autowired
 	private TransactionService transactionService;
+	
 	@GetMapping("/loans")
 	public List<LoanDTO> getLoans() {
 		return loanService.findAll().stream().map(loanName -> new LoanDTO(loanName)).collect(Collectors.toList());
+		//return clientLoanRepository.findByClient(clientRepository.findByEmail(authentication.getName())).stream().map(clientLoan -> new ApplicationDTO(clientLoan)).collect(Collectors.toList());
 	}
-	@Transactional
+	
+	
+	@RequestMapping(path = "/loans", method = RequestMethod.POST)
+	public ResponseEntity<Object> createLoan(@RequestBody LoanApplicationDTO loanApplicationDTO, Authentication authentication) {
+		
+		if (loanApplicationDTO == null) {
+			
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+		
+		Loan newLoan = loanService.findById(loanApplicationDTO.getLoanId()).orElse(null);
+		if (newLoan == null) {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+		Account account = accountService.findByNumber(loanApplicationDTO.getToAccountNumber());
+		if (!account.getClient().equals(clientService.findByEmail(authentication.getName()))) {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+		
+		
+		if (account == null) {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+		Transaction transaction = new Transaction(TransactionType.CREDIT, loanApplicationDTO.getAmount(), loanApplicationDTO.getToAccountNumber() + " loan approved", LocalDate.now(), account);
+		if (transaction.getAmount() > newLoan.getMaxAmount()) {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+		if (!newLoan.getPayments().contains(loanApplicationDTO.getPayments())) {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+		ClientLoan clientLoan = new ClientLoan(loanApplicationDTO.getAmount() * 1.2, loanApplicationDTO.getPayments());
+		clientLoan.setLoan(newLoan);
+		clientLoan.getLoan().setName(newLoan.getName());
+		clientLoan.setClient(clientService.findByEmail(authentication.getName()));
+		clientLoanService.save(clientLoan);
+		account.setBalance(account.getBalance()+ transaction.getAmount());
+		accountService.save(account);
+		transactionService.save(transaction);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+	
+}
+
+/*@GetMapping("/loans")
+	public List<LoanDTO> getLoans() {
+		return loanService.findAll().stream().map(loanName -> new LoanDTO(loanName)).collect(Collectors.toList());
+	}*/
+	/*@Transactional
 	@RequestMapping(path = "/loans", method = RequestMethod.POST)
 	public ResponseEntity<Object> createLoan(@RequestBody LoanApplicationDTO loanApplicationDTO, Authentication authentication) {
 		Loan currentLoan = loanService.getLoanById(loanApplicationDTO.getLoanId());
@@ -61,10 +111,10 @@ public class LoanController {
 		if (!currentClient.getAccounts().contains(destinyAccount)) {
 			return new ResponseEntity<>("Account not found in current client", HttpStatus.FORBIDDEN);
 		}
-		ClientLoan clientLoan = new ClientLoan(loanApplicationDTO.getAmount()*1.2, loanApplicationDTO.getPayments());
+		ClientLoan clientLoan = new ClientLoan(loanApplicationDTO.getAmount() * 1.2, loanApplicationDTO.getPayments());
 		
 		Transaction transaction = new Transaction(currentLoan.getName() + " - loan approved",
-			loanApplicationDTO.getAmount(), LocalDateTime.now(),TransactionType.CREDIT);
+			loanApplicationDTO.getAmount(), LocalDateTime.now(), TransactionType.CREDIT);
 		
 		destinyAccount.setBalance(destinyAccount.getBalance() + loanApplicationDTO.getAmount());
 		
@@ -84,9 +134,9 @@ public class LoanController {
 		@RequestMapping("/loans")
 		public List<LoanDTO> getLoans() {
 			return loanService.getLoans();
-		}
-	}
-}
+		}*/
+
+
 /*
 if (loanApplicationDTO == null) {
 			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
